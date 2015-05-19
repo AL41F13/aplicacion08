902677fedcd59e124b9deb1aeaae9388ff9a658a
AL41F13 <janeth982210@gmail.com>
2015-05-19 14:07:00 -0500

    uno
    juego

---
+++ js/acciones.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	$ ('#uno').on ('click',function(){
+		$('#resultado').append('1');
+	});
+	$('#dos').on ('click',function(){
+		$('#resultado').append('2');
+	});
+	$('#tres').on ('click',function(){
+		$('#resultado').append('3');
+	});
+	$ ('#cuatro').on ('click',function(){
+		$('#resultado').append('4');
+		
+	});
+	
+
+//}); 
+});
+
